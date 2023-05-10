e8bc973e7e50679b4d3a99ee98723b399f62edc0
msopansopian <m.sopian369@gmail.com>
2023-05-10 07:25:28 +0700

    commit pertama

---
+++ biodata.js
@@ -0,0 +1,10 @@
+function tampilkanBiodata(nama, tempatLahir, tanggalLahir, alamat) {
+  console.log("Nama: " + nama);
+  console.log("Tempat Lahir: " + tempatLahir);
+  console.log("Tanggal Lahir: " + tanggalLahir);
+  console.log("Alamat: " + alamat);
+}
+
+module.exports = {
+  tampilkanBiodata: tampilkanBiodata
+};
---
+++ moduleBiodata.js
@@ -0,0 +1,8 @@
+const biodata = require('./biodata');
+
+const nama = "M. Sopan Sopian";
+const tempatLahir = "Pandeglang";
+const tanggalLahir = "24 Mei 1998";
+const alamat = "Jl. Kp. Bojong, Tarikolot, Kec. Citeureup, Kab. Bogor, Jawa Barat";
+
+biodata.tampilkanBiodata(nama, tempatLahir, tanggalLahir, alamat);
